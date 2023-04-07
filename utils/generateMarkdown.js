e3c8ae1f33a5cadcdb967e4c3dfde7f0dc8d22c2
Tyrrance <tyrrancem37@gmail.com>
2023-04-06 22:08:50 -0400

    Created a function that returns a license badge based on which license is passed.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Created a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge;
+  if (license === 'IBM') {
+    badge = '![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)';
+  } else if (license === 'MIT') {
+    badge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+  } else if (license === 'ISC') {
+    badge = '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)';
+  } else {
+    badge = '';
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
